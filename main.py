fb04152e89cf39b8dc475280b9649999ce9f066c
Jayden Banks <jaydenbshelby@gmail.com>
2021-01-12 23:39:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import random
+
+UserInput = "Typer"
+ChatBot_Name = "Ace"
+print(": Hey there")
+UserInput = str.lower(input(": "))
+ignore_words = ["!", "?", "'"]
+if UserInput == ["who are you?","Hi","Whats up","who made you"]:
+ print(ChatBot_Name + ": My name is Ace, I am a chatbox created by Jayden Banks") 
+if UserInput == "how are you?":
+ print(ChatBot_Name + ": Im doing great")
+
+def generate_response(user_input):
+  responses = [
+    "How interesting!",
+    "You don't say!",
+    "Very cool!",
+    "Programming is fun!"
+  ]
+  return random.choice(responses)
+
+def init_chat():
+  Chatbot_Name = 'Ace'
+  quit_character = 'q'
+
+  user_input = input( Chatbot_Name + ": how are you?\n")
+
+  while user_input != quit_character:
+    #Ask the user for more input, then use that in your response
+    user_input = input(generate_response(user_input) + "\n")
+
+if __name__ == "__main__":
+  init_chat()
